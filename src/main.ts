be6ee297104728d757eca558d08aadb035f3bc4e
Ali Osman <106361546+osmandlsmn@users.noreply.github.com>
2022-12-09 11:00:54 +0300

    feat: add vuejs modal

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,15 @@ import Vue from "vue";
 
 import App from "./App.vue";
 import router from "./router";
+import store from "@/store/index";
+import VModal from "vue-js-modal";
 
 import "@/styles/tailwind.css";
 
+Vue.use(VModal);
+
 new Vue({
   router,
+  store,
   render: (h) => h(App),
 }).$mount("#app");
