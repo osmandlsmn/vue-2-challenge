f57dfd431c110e1dc7bef6f4b39dc2d8271c9432
Ali Osman <106361546+osmandlsmn@users.noreply.github.com>
2022-12-09 11:00:36 +0300

    style: update colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#0F6FE9",
+        secondary: "#768DDE",
+        danger: "##FF1C1C",
+        success: "#0EDC79",
+      },
+    },
   },
   plugins: [],
 };
