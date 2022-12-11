8b8ff38f81883c4c4ec6a5503efb5c2941941725
Ali Osman <106361546+osmandlsmn@users.noreply.github.com>
2022-12-11 03:55:15 +0300

    feat: add input, button component

---
+++ src/components/UI/Button/index.vue
@@ -0,0 +1,32 @@
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Button",
+  props: {
+    primary: Boolean,
+    secondary: Boolean,
+    success: Boolean,
+    danger: Boolean,
+  },
+  computed: {
+    getVariant() {
+      if (this.$props.primary) return "bg-primary text-white";
+      else if (this.$props.secondary) return "bg-secondary text-white";
+      else if (this.$props.success) return "bg-success text-white";
+      else if (this.$props.danger) return "bg-danger text-white";
+    },
+  },
+  methods: {
+    onClick(event: any) {
+      this.$emit("onClick", event);
+    },
+  },
+});
+</script>
+
+<template>
+  <button :class="`font-medium rounded-lg text-sm px-5 py-2.5 text-center ${getVariant}`" @click="onClick">
+    <slot />
+  </button>
+</template>
---
+++ src/components/UI/Input/index.vue
@@ -0,0 +1,27 @@
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  inheritAttrs: false,
+  name: "Input",
+  props: {
+    placeholder: String,
+  },
+  methods: {
+    changeValue(event: any) {
+      this.$emit("input", event.target.value);
+    },
+  },
+});
+</script>
+
+<template>
+  <div>
+    <input
+      type="text"
+      class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+      :placeholder="placeholder"
+      @input="changeValue"
+    />
+  </div>
+</template>
