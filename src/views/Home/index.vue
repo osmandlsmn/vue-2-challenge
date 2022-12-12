745f7186b6d829f0816174bd4f8d2346e3d723ba
Ali Osman <106361546+osmandlsmn@users.noreply.github.com>
2022-12-12 17:37:10 +0300

    feat: update home page

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -22,7 +22,7 @@ export default defineComponent({
     <Actions />
     <hr class="mt-3 mb-3" />
     <div class="flex justify-between">
-      <Stocks class="grow-2" />
+      <Stocks />
       <PieCharts />
     </div>
   </div>
