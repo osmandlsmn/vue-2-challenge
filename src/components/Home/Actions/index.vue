75e91d57f196e9f34100d97c28df8b045bdd3952
Ali Osman <106361546+osmandlsmn@users.noreply.github.com>
2022-12-12 03:07:44 +0300

    feat: add actions component

---
+++ src/components/Home/Actions/index.vue
@@ -0,0 +1,23 @@
+<script lang="ts">
+import { defineComponent } from "vue";
+import Button from "@/components/UI/Button/index.vue";
+
+export default defineComponent({
+  name: "Actions",
+  components: {
+    Button,
+  },
+  methods: {
+    openAddStackModal() {
+      this.$modal.show("add-stock-modal");
+    },
+  },
+});
+</script>
+
+<template>
+  <div class="space-x-5">
+    <Button @onClick="openAddStackModal" primary>Add Stock</Button>
+    <Button primary>Refresh</Button>
+  </div>
+</template>
