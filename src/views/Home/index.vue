ce3a4f552156fcd0e01438165ff3ee25d13a976e
Ali Osman <106361546+osmandlsmn@users.noreply.github.com>
2022-12-12 03:08:06 +0300

    feat: update home page

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,3 +1,29 @@
+<script lang="ts">
+import Stocks from "@/components/Home/Stocks/index.vue";
+import PieCharts from "@/components/Home/PieCharts/index.vue";
+import AddStockModal from "@/components/Home/AddStockModal/index.vue";
+import Actions from "@/components/Home/Actions/index.vue";
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Home",
+  components: {
+    Stocks,
+    PieCharts,
+    AddStockModal,
+    Actions,
+  },
+});
+</script>
+
 <template>
-  <div class="">Hello World</div>
+  <div class="py-10 px-10">
+    <AddStockModal />
+    <Actions />
+    <hr class="mt-3 mb-3" />
+    <div class="flex justify-between">
+      <Stocks class="grow-2" />
+      <PieCharts />
+    </div>
+  </div>
 </template>
